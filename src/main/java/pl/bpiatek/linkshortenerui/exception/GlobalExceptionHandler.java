fb0230941e1067d80f0039618fe725a2165062cb
Bartosz Piatek <your.email@example.com>
2025-12-16 10:01:09 +0100

    fix exception handler

--- src/main/java/pl/bpiatek/linkshortenerui/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/bpiatek/linkshortenerui/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package pl.bpiatek.linkshortenerui.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
@@ -11,11 +12,15 @@ public class GlobalExceptionHandler {
 
     Logger log = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
-    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
-    public String handleUnauthorized() {
-        log.info("GlobalExceptionHandler caught 401 - Redirecting to login");
-        // If we end up here, it means Access Token expired AND Refresh Token failed.
-        // Force user to login again.
-        return "redirect:/login";
+    @ExceptionHandler(HttpClientErrorException.class)
+    public String handleHttpClientError(HttpClientErrorException ex) {
+
+        // Check if it is actually a 401
+        if (ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+            log.info("GlobalExceptionHandler caught 401 - Redirecting to login");
+            return "redirect:/login";
+        }
+
+        throw ex;
     }
 }
