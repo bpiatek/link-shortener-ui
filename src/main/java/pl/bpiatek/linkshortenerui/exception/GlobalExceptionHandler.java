080fbdb7111755824c0570a3e88c24785f9b92ec
Bartosz Piatek <your.email@example.com>
2025-12-16 09:56:25 +0100

    added logs when unauthorized

--- src/main/java/pl/bpiatek/linkshortenerui/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/bpiatek/linkshortenerui/exception/GlobalExceptionHandler.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.logging.Logger;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    Logger log = Logger.getLogger(GlobalExceptionHandler.class.getName());
+
     @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
     public String handleUnauthorized() {
+        log.info("GlobalExceptionHandler caught 401 - Redirecting to login");
         // If we end up here, it means Access Token expired AND Refresh Token failed.
         // Force user to login again.
         return "redirect:/login";
