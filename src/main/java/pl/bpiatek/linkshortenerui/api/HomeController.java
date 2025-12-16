7c76e35d831e26396889a2c0abe152515e411158
Bartosz Piatek <your.email@example.com>
2025-12-16 13:22:20 +0100

    correctly logout in dashboard

--- src/main/java/pl/bpiatek/linkshortenerui/api/HomeController.java
+++ src/main/java/pl/bpiatek/linkshortenerui/api/HomeController.java
@@ -1,13 +1,19 @@
 package pl.bpiatek.linkshortenerui.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class HomeController {
 
     @GetMapping("/")
-    String home() {
+    String home(@CookieValue(value = "jwt", required = false) String jwt) {
+        if (jwt != null) {
+
+            return "redirect:/dashboard";
+        }
+
         return "index";
     }
 }
