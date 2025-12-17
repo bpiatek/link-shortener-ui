867177f4b02f0aee226585c23e3e2b9c404ffa83
Bartosz Piatek <your.email@example.com>
2025-12-17 16:41:43 +0100

    fix pagination

--- src/main/java/pl/bpiatek/linkshortenerui/dto/PageResponse.java
+++ src/main/java/pl/bpiatek/linkshortenerui/dto/PageResponse.java
@@ -8,4 +8,16 @@ public record PageResponse<T>(
         int size,
         long totalElements,
         int totalPages
-) {}
+) {
+    public boolean isFirst() {
+        return page == 0;
+    }
+
+    public boolean isLast() {
+        return page >= totalPages - 1;
+    }
+
+    public boolean isEmpty() {
+        return content == null || content.isEmpty();
+    }
+}
