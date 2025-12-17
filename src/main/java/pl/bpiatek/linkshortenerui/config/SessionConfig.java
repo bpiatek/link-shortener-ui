6eaf6801de17abf7e9f19cbb5c994b7d2594521d
Bartosz Piatek <your.email@example.com>
2025-12-17 18:46:07 +0100

    turn off session

--- src/main/java/pl/bpiatek/linkshortenerui/config/SessionConfig.java
+++ src/main/java/pl/bpiatek/linkshortenerui/config/SessionConfig.java
@@ -1,20 +1,34 @@
 package pl.bpiatek.linkshortenerui.config;
 
 import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Configuration
 class SessionConfig {
 
     @Bean
-    public Filter sessionDisablingFilter() {
-        return (request, response, chain) -> {
-            if (request instanceof HttpServletRequest req) {
-                req.getSession(false);
+    public FilterRegistrationBean<OncePerRequestFilter> disableSessionFilter() {
+        var registration = new FilterRegistrationBean<OncePerRequestFilter>();
+        registration.setFilter(new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain filterChain)
+                    throws ServletException, IOException {
+                request.getSession(false);
+                filterChain.doFilter(request, response);
             }
-            chain.doFilter(request, response);
-        };
+        });
+        registration.setOrder(Integer.MIN_VALUE);
+        return registration;
     }
 }
